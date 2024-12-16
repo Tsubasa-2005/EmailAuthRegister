0702b75ce2152784519229b65932f531ac426acc
Tsubasa-2005 <a.t.s.y.i.2314@icloud.com>
2024-12-16 22:54:26 +0900

    :sparkles: main.go

---
+++ backend/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"context"
+	"log/slog"
+	"os"
+
+	"github.com/Tsubasa-2005/EmailAuthResister/cmd"
+)
+
+func main() {
+	var slogOpts slog.HandlerOptions
+	slogOpts.Level = slog.LevelDebug
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, &slogOpts))
+	slog.SetDefault(logger)
+
+	ctx := context.Background()
+
+	if err := cmd.RootCmd(ctx).Execute(); err != nil {
+		slog.ErrorContext(ctx, err.Error())
+		os.Exit(1)
+	}
+}
