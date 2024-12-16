ed295a3ef8764705d16839292d05212072680dc7
Tsubasa-2005 <a.t.s.y.i.2314@icloud.com>
2024-12-16 22:53:51 +0900

    :sparkles: register handler, and test

---
+++ backend/usesase/handler/register.go
@@ -0,0 +1,61 @@
+package handler
+
+import (
+	"context"
+
+	"github.com/Tsubasa-2005/EmailAuthResister/pkg/infra/rdb"
+	"github.com/Tsubasa-2005/EmailAuthResister/pkg/util"
+	"github.com/Tsubasa-2005/EmailAuthResister/ui/api"
+	"github.com/go-faster/errors"
+	"github.com/jackc/pgx/v5/pgtype"
+)
+
+func (h *Handler) CompleteUserRegistration(ctx context.Context, req *api.CompleteUserRegistrationReq) (api.CompleteUserRegistrationRes, error) {
+	verification, err := h.repo.GetEmailVerificationToken(ctx, pgtype.UUID{
+		Bytes: req.Token,
+		Valid: true,
+	})
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to get email verification info")
+	}
+
+	if verification.ExpiresAt.Time.Before(util.NowJST()) {
+		return &api.Unauthorized{
+			Message: "expired token",
+		}, nil
+	}
+
+	if verification.Email != req.Email {
+		return &api.BadRequest{
+			Message: "invalid email",
+		}, nil
+	}
+
+	hashPass, err := util.GeneratePasswordHash(req.Password)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to generate password hash")
+	}
+
+	user, err := h.repo.CreateUser(ctx, rdb.CreateUserParams{
+		Email:     req.Email,
+		Password:  hashPass,
+		Name:      req.Name,
+		CreatedAt: util.Timestamptz(util.NowJST()),
+		UpdatedAt: util.Timestamptz(util.NowJST()),
+	})
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to update user")
+	}
+
+	token, err := util.GenerateToken("EmailAuthRegister", util.UserToken{
+		ID:   user.ID,
+		Name: user.Name,
+	})
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to generate token")
+	}
+
+	return &api.CompleteUserRegistrationOK{
+		Token: token,
+	}, nil
+}
---
+++ backend/usesase/handler/register_test.go
@@ -0,0 +1,130 @@
+package handler_test
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"io"
+	"net/http"
+	"testing"
+	"time"
+
+	"github.com/Tsubasa-2005/EmailAuthResister/pkg/infra"
+	"github.com/Tsubasa-2005/EmailAuthResister/pkg/infra/rdb"
+	"github.com/Tsubasa-2005/EmailAuthResister/pkg/testutil"
+	"github.com/Tsubasa-2005/EmailAuthResister/pkg/testutil/fixture"
+	"github.com/Tsubasa-2005/EmailAuthResister/pkg/util"
+	"github.com/Tsubasa-2005/EmailAuthResister/ui/api"
+	"github.com/Tsubasa-2005/EmailAuthResister/usesase/handler"
+	"github.com/go-faker/faker/v4"
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/stretchr/testify/require"
+)
+
+func TestHandler_CompleteUserRegistration(t *testing.T) {
+	t.Parallel()
+
+	ctx := context.Background()
+
+	dbConn := infra.ConnectDB(ctx)
+
+	t.Run("Invalid token", func(t *testing.T) {
+		t.Parallel()
+
+		verification := fixture.CreateEmailVerificationToken(t, ctx, dbConn, func(target *rdb.TestCreateEmailVerificationTokenParams) {
+			(*target).ExpiresAt = util.Timestamptz(util.NowJST().Add(-24 * time.Hour))
+		})
+
+		postParams := api.CompleteUserRegistrationReq{
+			Token:    verification.Token.Bytes,
+			Email:    verification.Email,
+			Name:     faker.Name(),
+			Password: faker.Password(),
+		}
+		body, err := postParams.MarshalJSON()
+		require.NoError(t, err)
+
+		resp := testutil.SetupAndRequest(t, ctx, http.MethodPost, "/complete-registration", "", bytes.NewBuffer(body))
+		defer func(Body io.ReadCloser) {
+			err := Body.Close()
+			require.NoError(t, err)
+		}(resp.Body)
+		require.Equal(t, http.StatusUnauthorized, resp.StatusCode)
+		body, err = io.ReadAll(resp.Body)
+		require.NoError(t, err)
+
+		var res401 api.Unauthorized
+		require.NoError(t, json.Unmarshal(body, &res401))
+
+		require.Equal(t, "expired token", res401.Message)
+	})
+
+	t.Run("Invalid email", func(t *testing.T) {
+		t.Parallel()
+
+		verification := fixture.CreateEmailVerificationToken(t, ctx, dbConn, nil)
+
+		postParams := api.CompleteUserRegistrationReq{
+			Token:    verification.Token.Bytes,
+			Email:    faker.Email(),
+			Name:     faker.Name(),
+			Password: faker.Password(),
+		}
+		body, err := postParams.MarshalJSON()
+		require.NoError(t, err)
+
+		resp := testutil.SetupAndRequest(t, ctx, http.MethodPost, "/complete-registration", "", bytes.NewBuffer(body))
+		defer func(Body io.ReadCloser) {
+			err := Body.Close()
+			require.NoError(t, err)
+		}(resp.Body)
+		require.Equal(t, http.StatusBadRequest, resp.StatusCode)
+		body, err = io.ReadAll(resp.Body)
+		require.NoError(t, err)
+
+		var res400 api.BadRequest
+		require.NoError(t, json.Unmarshal(body, &res400))
+
+		require.Equal(t, "invalid email", res400.Message)
+	})
+
+	t.Run("Success", func(t *testing.T) {
+		t.Parallel()
+
+		verification := fixture.CreateEmailVerificationToken(t, ctx, dbConn, nil)
+
+		postParams := api.CompleteUserRegistrationReq{
+			Token:    verification.Token.Bytes,
+			Email:    verification.Email,
+			Name:     faker.Name(),
+			Password: faker.Password(),
+		}
+		body, err := postParams.MarshalJSON()
+		require.NoError(t, err)
+
+		resp := testutil.SetupAndRequest(t, ctx, http.MethodPost, "/complete-registration", "", bytes.NewBuffer(body))
+		defer func(Body io.ReadCloser) {
+			err := Body.Close()
+			require.NoError(t, err)
+		}(resp.Body)
+		require.Equal(t, http.StatusOK, resp.StatusCode)
+		body, err = io.ReadAll(resp.Body)
+		require.NoError(t, err)
+
+		var res200 api.CompleteUserRegistrationOK
+		require.NoError(t, json.Unmarshal(body, &res200))
+
+		token, err := util.ParseToken(res200.Token)
+		require.NoError(t, err)
+
+		claims := token.Claims.(jwt.MapClaims)
+
+		id, err := handler.GetInt64FromClaims(claims, "user_id")
+		require.NoError(t, err)
+
+		_, ok := claims["name"].(string)
+		require.True(t, ok)
+
+		require.NoError(t, rdb.New(dbConn).TestDeleteUser(ctx, id))
+	})
+}
