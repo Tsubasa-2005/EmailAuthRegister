7e6ed9fb8f59dfa4b901b5e394b57cd34b51e9cb
Tsubasa-2005 <a.t.s.y.i.2314@icloud.com>
2024-12-16 22:45:12 +0900

    :sparkles: email_verification_tokens

---
+++ backend/db/queries/email_verification_tokens.sql
@@ -0,0 +1,11 @@
+-- name: GetEmailVerificationToken :one
+select * from email_verification_tokens where id = $1;
+
+-- name: GetEmailVerificationTokenByEmail :one
+select * from email_verification_tokens where email = $1;
+
+-- name: CreateEmailVerificationToken :one
+insert into email_verification_tokens (token, email, expires_at, created_at) values ($1, $2, $3, $4) returning *;
+
+-- name: DeleteEmailVerificationToken :exec
+delete from email_verification_tokens where token = $1;
