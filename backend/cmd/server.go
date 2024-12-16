7e2c9ae9d73eb9be9d2f35e050607a46b0505288
Tsubasa-2005 <a.t.s.y.i.2314@icloud.com>
2024-12-16 22:54:20 +0900

    :sparkles: cmd server

---
+++ backend/cmd/server.go
@@ -0,0 +1,56 @@
+package cmd
+
+import (
+	"context"
+	"log/slog"
+	"net/http"
+	"os"
+
+	"github.com/Tsubasa-2005/EmailAuthResister/pkg/gmail"
+	"github.com/Tsubasa-2005/EmailAuthResister/pkg/infra"
+	"github.com/Tsubasa-2005/EmailAuthResister/pkg/middleware"
+	"github.com/Tsubasa-2005/EmailAuthResister/ui/api"
+	"github.com/Tsubasa-2005/EmailAuthResister/usesase/handler"
+	"github.com/go-faster/errors"
+	"github.com/spf13/cobra"
+)
+
+func serverCmd(ctx context.Context) *cobra.Command {
+	return &cobra.Command{
+		Use:   "serve",
+		Short: "Start the HTTP server",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return runServer(ctx)
+		},
+	}
+}
+
+func runServer(ctx context.Context) error {
+	dbConn := infra.ConnectDB(ctx)
+	hostname := "smtp.gmail.com"
+	port := 587
+	username := os.Getenv("SMTP_USERNAME")
+	password := os.Getenv("SMTP_PASSWORD")
+
+	gmailClient, err := gmail.NewConnect(hostname, port, username, password)
+	if err != nil {
+		return errors.Wrap(err, "failed to create Gmail client")
+	}
+	h := handler.NewHandler(dbConn, gmailClient)
+	s := handler.NewSecurityHandler()
+
+	srv, err := api.NewServer(h, s, api.WithMiddleware(middleware.Logging()))
+	if err != nil {
+		return err
+	}
+
+	corsHandler := middleware.Cors(srv)
+
+	addr := ":8080"
+	slog.InfoContext(ctx, "Starting HTTP Server", slog.String("addr", addr))
+	if err := http.ListenAndServe(addr, corsHandler); err != nil {
+		return err
+	}
+
+	return nil
+}
