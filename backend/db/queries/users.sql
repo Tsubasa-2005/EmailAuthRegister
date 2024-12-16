e51ae993f1745de16dae4a38b5e8f9a32e8dc5fe
Tsubasa-2005 <a.t.s.y.i.2314@icloud.com>
2024-12-16 22:45:27 +0900

    :sparkles: users.sql

---
+++ backend/db/queries/users.sql
@@ -0,0 +1,20 @@
+-- name: GetUsers :many
+select * from users where id = $1 and deleted_at is null;
+
+-- name: GetUsersByEmail :one
+select * from users where email = $1 and deleted_at is null;
+
+-- name: ExistsUserByEmail :one
+select exists(select 1 from users where email = $1 and deleted_at is null);
+
+-- name: CreateUser :one
+insert into users (email, password, name, created_at, updated_at) values ($1, $2, $3, $4, $5) returning *;
+
+-- name: UpdateUser :one
+update users set email = $1, password = $2, name = $3, updated_at = $4 where id = $5 returning *;
+
+-- name: UpdateUserByEmail :one
+update users set password = $1, name = $2, updated_at = $3 where email = $4 returning *;
+
+-- name: SoftDeleteUsers :exec
+update users set deleted_at = $1 where id = $2;
