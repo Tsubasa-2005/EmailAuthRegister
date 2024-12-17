bdaa825537878674e3d65bfc57b16a1097d16c95
Tsubasa-2005 <a.t.s.y.i.2314@icloud.com>
2024-12-17 15:15:38 +0900

    :sparkles: home page, layout

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import React from "react";
+import Header from "@/app/components/Header";
+import Footer from "@/app/components/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,12 +26,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className="antialiased flex flex-col min-h-screen">
+      <Header/>
+      <main className="flex-grow">{children}</main>
+      <Footer/>
       </body>
-    </html>
+      </html>
   );
 }
