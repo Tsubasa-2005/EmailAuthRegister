1fba44c53bbaf351cd51dc0b15cd0969f4d247b9
Tsubasa-2005 <a.t.s.y.i.2314@icloud.com>
2024-12-17 15:13:58 +0900

    :sparkles: ping endpoint

---
+++ frontend/api/ping/api.ts
@@ -0,0 +1,17 @@
+import apiClient from "@/api/client";
+
+export async function pingAPI(): Promise<{
+    success: boolean;
+}> {
+    try {
+        await apiClient.ping();
+
+        return {
+            success: true,
+        };
+    } catch {
+        return {
+            success: false,
+        };
+    }
+}
