9d9c66b5210a7e7229564c0d96b688be933519de
Tsubasa-2005 <a.t.s.y.i.2314@icloud.com>
2024-12-17 15:10:56 +0900

    :sparkles: code gen

---
+++ frontend/generated/models/index.ts
@@ -0,0 +1,10 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './CompleteUserRegistration200Response';
+export * from './CompleteUserRegistrationRequest';
+export * from './Ping200Response';
+export * from './SendEmailVerification500Response';
+export * from './SendEmailVerificationRequest';
+export * from './User';
+export * from './VerifyEmail200Response';
+export * from './VerifyEmailRequest';
