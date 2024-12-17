f0fd85f61fbfa67b35fbf394438a257c6516e6d2
Tsubasa-2005 <a.t.s.y.i.2314@icloud.com>
2024-12-17 15:14:21 +0900

    :sparkles: send-verification endpoint, postHelper

---
+++ frontend/api/send-verification/api.ts
@@ -0,0 +1,25 @@
+import apiClient from "@/api/client";
+import {SendEmailVerificationOperationRequest} from "@/generated";
+import {handleError} from "@/api/error_message";
+
+export async function postSendVerificationAPI(
+    requestParams: SendEmailVerificationOperationRequest
+): Promise<{
+    success: boolean;
+    message: string;
+}> {
+    try {
+        await apiClient.sendEmailVerification(requestParams);
+
+        return {
+            success: true,
+            message: "",
+        };
+    } catch (error) {
+        const { message } = await handleError(error);
+        return {
+            success: false,
+            message,
+        };
+    }
+}
---
+++ frontend/api/send-verification/postHelper.ts
@@ -0,0 +1,24 @@
+import {postSendVerificationAPI} from "@/api/send-verification/api";
+import {SuccessMessageParams} from "@/api/types";
+import {Dispatch, SetStateAction} from "react";
+import {SendEmailVerificationOperationRequest} from "@/generated";
+import {successMessageHelper} from "@/api/success_message";
+import {unexpectedErrorMessage} from "@/api/error_message";
+
+export async function postSendVerificationHelper(
+    params: SendEmailVerificationOperationRequest,
+    setError: Dispatch<SetStateAction<string | null>>,
+    successParams: SuccessMessageParams,
+): Promise<void> {
+    try {
+        const data = await postSendVerificationAPI(params);
+
+        if (data.success) {
+            successMessageHelper(successParams);
+        } else {
+            setError(data.message);
+        }
+    } catch {
+        setError(unexpectedErrorMessage);
+    }
+}
