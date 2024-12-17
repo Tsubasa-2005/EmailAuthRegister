a302aa95833a9ac9ecc6e5740f49f4ff91d99060
Tsubasa-2005 <a.t.s.y.i.2314@icloud.com>
2024-12-17 15:16:19 +0900

    :sparkles: users page

---
+++ frontend/app/users/page.tsx
@@ -0,0 +1,7 @@
+export default function UsersPage() {
+    return (
+        <div className="min-h-screen flex items-center justify-center bg-gray-50">
+            <h1 className="text-4xl font-extrabold text-gray-800">Welcome</h1>
+        </div>
+    );
+}
