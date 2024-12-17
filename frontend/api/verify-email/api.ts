2f4171c559a940113e3383a6172d2c68ba21617e
Tsubasa-2005 <a.t.s.y.i.2314@icloud.com>
2024-12-17 15:14:34 +0900

    :sparkles: verify-email endpoint, postHelper

---
+++ frontend/api/verify-email/api.ts
@@ -0,0 +1,31 @@
+import { VerifyEmailOperationRequest} from "@/generated";
+import apiClient from "@/api/client";
+import {handleError} from "@/api/error_message";
+
+export async function postVerifyEmailAPI(
+    requestParams: VerifyEmailOperationRequest
+): Promise<{
+    success: boolean;
+    email: string;
+    message: string;
+    status: number;
+}> {
+    try {
+        const response = await apiClient.verifyEmail(requestParams);
+
+        return {
+            success: true,
+            email: response.email,
+            message: "",
+            status: 200,
+        };
+    } catch (error) {
+        const { message, status } = await handleError(error);
+        return {
+            success: false,
+            email: "",
+            message,
+            status,
+        };
+    }
+}
