8a32892065b02716e23eadfd3b02db7f4781b606
Tsubasa-2005 <a.t.s.y.i.2314@icloud.com>
2024-12-17 16:29:52 +0900

    :zap: users.sql

--- backend/db/queries/users.sql
+++ backend/db/queries/users.sql
@@ -1,11 +1,14 @@
 -- name: GetUsers :many
-select * from users where id = $1 and deleted_at is null;
+select * from users where deleted_at is null limit $1 offset $2;
 
--- name: GetUsersByEmail :one
+-- name: GetUserByEmail :one
 select * from users where email = $1 and deleted_at is null;
 
 -- name: ExistsUserByEmail :one
 select exists(select 1 from users where email = $1 and deleted_at is null);
+
+-- name: CountActiveUsers :one
+select count(*) from users where deleted_at is null;
 
 -- name: CreateUser :one
 insert into users (email, password, name, created_at, updated_at) values ($1, $2, $3, $4, $5) returning *;
